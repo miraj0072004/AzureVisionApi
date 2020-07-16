4e1d84aeab9b26b34a5d8cbad40570fba1438344
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-16 21:08:17 +1000

    Setup the project to use the face api

--- src/config.ts
+++ src/config.ts
@@ -5,5 +5,12 @@ export let config = {
         endPoint : "https://mirajvisionapi.cognitiveservices.azure.com/vision/v3.0/",
         key1: "c3f6f9a36cf741cebaa8c20a1ca6df72",
         key2: "333b661ef1d84852998257001c5a6458"
+    },
+
+    face:
+    {
+        endPoint : "https://mirajfaceapi.cognitiveservices.azure.com/face/v1.0/",
+        key1: "8f1873d241eb459bad2380484938370b",
+        key2: "86c461222afe4f17b6b439937494e2c1"
     }
 };
