3f473d689c7394de4373d0e6324416cd11a5e5eb
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-07-22 13:09:10 +1000

    Create a person group

--- src/faceHelpers.ts
+++ src/faceHelpers.ts
@@ -4,3 +4,32 @@ import * as querystring from 'querystring';
 import * as fileHelpers from './fileHelpers';
 import {setInterval, clearInterval} from 'timers';
 
+// Creates a person group and returns personGroupId
+export function createPersonGroup(personGroupId: string): Promise<string> {
+    const promise = new Promise<string>((resolve, reject) => {
+        const requestOptions = getRequestOptions();
+        requestOptions.headers['Content-Type'] = 'application/json';
+        requestOptions.body = JSON.stringify({
+            'name': personGroupId
+        });
+
+        request.put(
+            config.face.endPoint + '/persongroups/' + personGroupId,
+            requestOptions, 
+            (err, response, body) => {
+                if (err) { reject(false); }
+                else { resolve(personGroupId); }                
+            }
+        )
+    });
+    return promise;
+}
+
+function getRequestOptions(): request.CoreOptions {
+    return {
+        headers: {
+            "Content-Type": "application/octet-stream",
+            "Ocp-Apim-Subscription-Key": config.face.key1
+        }
+    };
+}
